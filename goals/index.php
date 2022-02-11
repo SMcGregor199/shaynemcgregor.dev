34474188ee15bff1c90c648aa640247c8b1087ef
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-02-11 15:28:37 -0500

    change back to php

---
+++ goals/index.php
@@ -0,0 +1,14 @@
+<h1>Goals</h1>
+<h2>As of Feb 11th, 2022</h2>
+
+<ul>
+	<li>To earn a job working in web development by the summer of 2022</li>
+	<li>To learn Javascript</li>
+	<li>To get better at markdown</li>
+	<li>To create and maintain a blog focused on web dev and hip hop </li>
+	<li>To become a better pixel art artist</li>
+	<li>Learn ways to incorporate my pixel art into my web development</li>
+	<li>To help build my newphew's website for his T-shirt business.</li>
+	<li>to graduate with my Ph.D. in May 2022</li>
+</ul>
+
