5bb44b3362f1fd63de007eee6a6f4961072ecac4
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-02-12 10:39:47 -0500

    edit project card and styles for proejct card

--- temp/modules/project-card.php
+++ temp/modules/project-card.php
@@ -1,15 +1,17 @@
 <project-card>
-	<picture class = "thumbnail">
-		<img src='<?=$project["thumbnail"]?>'>
-	</picture>
+	<project-heading>
+		<picture class = "thumbnail">
+			<img src='<?=$project["thumbnail"]?>'>
+		</picture>
 
-	<a href=""><p class = "important-voice"><?=$project["title"]?></a></p>
+		<a href=""><p class = "important-voice"><?=$project["title"]?></a></p>
+	</project-heading>
 
-	<div class="project-info">
+	<project-info>
 		<span class="important-voice">Goal:</span><p class="base-voice"><?=$project["goal"]?></p>
 
 		<span class="important-voice">Description:</span><p class="base-voice"><?=$project["description"]?></p>
 
 		<span class="important-voice">Process:</span><p class="base-voice"><?=$project["process"]?></p>
-	</div>
+	<project-info>
 </project-card>
--- temp/modules/projects-database.php
+++ temp/modules/projects-database.php
@@ -1,24 +1,24 @@
 <?php
 
 $projects_database = [
-	[
+	[	"thumbnail" => "modules/images/popcorn.jpg",
 		"title" => "Responsive Layout Popcorn Website",
 		"description" => "This is a site for fans of popcorn in all its deliciousness. It contains a header, a welcome section, some profile cards, and a footer",
-		"goals" => "To  create a website that looks good and is responsive on every viewport size",
+		"goal" => "To  create a website that looks good and is responsive on every viewport size",
 		"process" => "Used the Flex Box layout module and changed its property values at different viewport sizes using media queries",
 	],
 
-	[
+	[	"thumbnail" => "modules/images/tech-site.jpg",
 		"title" => "Tech Brochure Website",
 		"description" => "This is a brochure website for a life-style VR tech device. It's responsive at every viewport ad contains buttons and links with hover states. ",
-		"goals" => "To style a website utilizing a prebuilt base code without changing the HTML. ",
+		"goal" => "To style a website utilizing a prebuilt base code without changing the HTML. ",
 		"process" => "Using my knowledge of CSS and PHP (the base code is a collection of php modules)  I  navigated through multiple files to style the basecode without touching the HTML. I kept track of all my changes by using comments and writing my code at the bottome of the main css document.",
 	],
 
-	[
+	[	"thumbnail" => "modules/images/exercises.jpg",
 		"title" => "Forms with PHP",
 		"description" => "This project is a collection of different forms generated using PHP. Some programs will create a madlib. Other forms will create will display mathmatical calculations. ",
-		"goals" => "Create the practice programs using the guide from Brian Hogan's Exercises for Programmers (2015)",
+		"goal" => "Create the practice programs using the guide from Brian Hogan's Exercises for Programmers (2015)",
 		"process" => "Combined PHP and HTML to produce a form that on submission produces a series of outputs based on conditional php statements",
 	],
 
