4d5d18f5dd115a519745fc1aa9c5c7155dfb225a
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-02-14 07:09:23 -0500

    add grid to projects section

--- temp/modules/blog-intro.php
+++ temp/modules/blog-intro.php
@@ -3,7 +3,7 @@
 		<h1 class = "attention-voice">Writing</h1>
 
 		<p class = "intro-voice">I mostly write on Substack. But you can find me on Codenewbie where I typically respost my writing. Here are some of the pieces I'm particularly proud of. and speak to what I've been learning about web development.</p>
-</blog-heading>
+	</blog-heading>
 
 	<blog-grid>	
 		<?php include('blogs-database.php'); ?>
