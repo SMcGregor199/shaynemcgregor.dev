beef1ab41ae96fc6cd82e0dd94fe9f54b64cf649
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-02-14 06:36:35 -0500

    medium viewport size

--- temp/modules/blog-intro.php
+++ temp/modules/blog-intro.php
@@ -1,7 +1,9 @@
 <blog-intro>
-	<h1 class = "attention-voice">Writing</h1>
+	<blog-heading>
+		<h1 class = "attention-voice">Writing</h1>
 
-	<p class = "intro-voice">I mostly write on Substack. But you can find me on Codenewbie where I typically respost my writing. Here are some of the pieces I'm particularly proud of. and speak to what I've been learning about web development.</p>
+		<p class = "intro-voice">I mostly write on Substack. But you can find me on Codenewbie where I typically respost my writing. Here are some of the pieces I'm particularly proud of. and speak to what I've been learning about web development.</p>
+</blog-heading>
 
 	<blog-grid>	
 		<?php include('blogs-database.php'); ?>
