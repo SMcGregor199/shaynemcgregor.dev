a790226982d26988ef0f2bf578049a0bea87ef89
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-02-14 07:28:34 -0500

    medium viewport changes

--- temp/modules/playlist-intro.php
+++ temp/modules/playlist-intro.php
@@ -1,8 +1,10 @@
 <playlist-intro>
-	<h1 class = "attention-voice">Playlist</h1>
+	<playlist-heading>
+		<h1 class = "attention-voice">Playlist</h1>
 
-	<p class = "intro-voice">This is a collection of tracks I've been listening to.  It's a dope list of tracks if you're into a mix of lofi, drill, phonk remixes, and '90s era beats. I'm still wrapping my head around how the internet has changed rap music. If you have any thoughts on this topic, comment on my Substack.</p>
-
+		<p class = "intro-voice">This is a collection of tracks I've been listening to.  It's a dope list of tracks if you're into a mix of lofi, drill, phonk remixes, and '90s era beats. I'm still wrapping my head around how the internet has changed rap music. If you have any thoughts on this topic, comment on my Substack.</p>
+	</playlist-heading>
+	
 	<playlist>
 		<picture class = "thumbnail">
 			<img src="modules/images/hip-hop-vibes.jpg">
