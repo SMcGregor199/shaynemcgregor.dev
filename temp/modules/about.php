6e2cbc3357eb7a51c82c67c9ef626675f65c762d
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-02-17 14:07:03 -0500

    large viewport about and writing section

--- temp/modules/about.php
+++ temp/modules/about.php
@@ -1,15 +1,19 @@
 <about>
+	<article>
+		<h1 class = "attention-voice">Who I Am</h1>
 
-	<h1 class = "attention-voice">Who I Am</h1>
+		<p class = "base-voice">I am a web designer and Ph.D. candidate at Yale University in African American Studies.</p>
+	</article>
 
-	<p class = "base-voice">I am a web designer and Ph.D. candidate at Yale University in African American Studies.</p>
+	<article>
+		<h1 class = "attention-voice">What I Do</h1>
 
-	<h1 class = "attention-voice">What I Do</h1>
+		<p class = "base-voice">I design websites. I listen to rap music. I write about both</p>
+	</article>
 
-	<p class = "base-voice">I design websites. I listen to rap music. I write about both</p>
-
-	<h1 class = "attention-voice">Goals</h1>
-	
-	<p class = "base-voice">To make cool and effective websites. To continue to write about rap music</p>
-
+	<article>
+		<h1 class = "attention-voice">Goals</h1>
+		
+		<p class = "base-voice">To make cool and effective websites. To continue to write about rap music</p>
+	</article>
 <about>
