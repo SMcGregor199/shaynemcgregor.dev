c1e5f6d00785d5426c2d25a70319ffc322e2205f
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-02-20 15:27:49 -0500

    finishing form

--- lion-cub-books/add-book.php
+++ lion-cub-books/add-book.php
@@ -3,24 +3,69 @@
 
 	$submitted = isset($_POST["submitted"]);
 
+	$title = null;
+	$author = null;
+	$blurb = null;
+	$genre = null;
+	$hasTitle = false;
+	$hasAuthor = false;
+	$hasBlurb = false;
+	$titleError = null;
+	$authorError = null;
+	$blurbError = null;
+	$hasGenre = null;
+
 	if($submitted) {
 
 		if( isset($_POST['title']) ){
 			$title = $_POST["title"];
+
+			if( strlen($title) > 0 ) {
+				$hasTitle = true;
+			} else {
+				$titleError = "please add the title";
+			}
 		}
 	
 		if( isset($_POST['author']) ){
 			$author = $_POST["author"];
+			
+			if( strlen($author) > 0 ) {
+				$hasAuthor = true;
+			} else {
+				$authorError = "please add the name of the author";
+
+			}
+
 		}
 
 		if( isset($_POST['blurb']) ){
 			$blurb = $_POST["blurb"];
+
+			if( strlen($blurb) > 0 ) {
+			//maybe change the string length on this? 
+				$hasBlurb = true; 
+			}  else {
+				$blurbError = "please add the blurb";
+
+			}
 		}
 
 		if( isset($_POST['genre']) ){
-			$genre = $_POST["genre"];
-		}
-		
+			$genre = intval($_POST["genre"]);
+			$hasGenre = true;
+		} 
+
+		if ($hasTitle && $hasAuthor &&$hasBlurb &&$hasGenre) {
+			$book = [
+				$title => $_POST["title"],
+				$author => $_POST["author"],
+				$blurb => $_POST["blurb"],
+				$genre => intval($_POST["genre"]),
+
+			];
+			var_dump($book);
+		}	
 	}
 ?>
 
@@ -30,23 +75,32 @@
 
 	<div class="field">
 		<label>Book Title</label>
-		<input name="title" value="" placeholder="...">
+		<input name="title" value="<?=$title?>" placeholder="...">
+		<?php if($titleError) {
+			echo $titleError;
+		} ?>
 	</div>
 
 	<div class="field">
 		<label>Author Name</label>
-		<input name="author" value="" placeholder="...">
+		<input name="author" value="<?=$author?>" placeholder="...">
+		<?php if($authorError) {
+			echo $authorError;
+		} ?>
 	</div>
 
 	<div class="field">
 		<label>Blurb</label>
-		<input name="blurb" value="" placeholder="...">
+		<input name="blurb" value="<?=$blurb?>" placeholder="...">
+		<?php if($blurbError) {
+			echo $blurbError;
+		} ?>
 	</div>
 
 	<div class="field">
 		<label>Genre</label>
 		<select name="genre">
-			<option value="">--Please choose an option--</option>
+			<option disabled>--Please choose an option--</option>
 			<option value="0">Biography & Memoir</option>
 			<option value="1">Literary Fiction</option>
 			<option value="2">Classics</option>
