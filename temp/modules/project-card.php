4dbd2b8a67360bcbb9bf947310c0bac65a805942
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-02-23 10:14:11 -0500

    changes to projects section

--- temp/modules/project-card.php
+++ temp/modules/project-card.php
@@ -1,5 +1,26 @@
-<project-card>
+<project-card class="test">
+
 	<heading>
+		<h1 class="important-voice"><?=$project["title"]?></h1>
+		<p class="intro-voice"><?=$project["category"]?></p>
+	</heading>
+
+	<description>
+		<p class="intro-voice"><?=$project["description"]?></p>
+	</description>
+
+	<thumbnail-link>
+		<picture class = "thumbnail">
+			<img src='<?=$project["thumbnail"]?>'>
+		</picture>
+
+		<a href="<?=$project['link']?>" target="<?=$project['title']?>" class="intro-voice">Check It Out!</a>
+	</thumbnail-link>
+	
+
+
+
+<!-- 	<heading>
 		<picture class = "thumbnail">
 			<img src='<?=$project["thumbnail"]?>'>
 		</picture>
@@ -13,5 +34,6 @@
 		<span class="important-voice">Description:</span><p class="intro-voice"><?=$project["description"]?></p>
 
 		<span class="important-voice">Process:</span><p class="intro-voice"><?=$project["process"]?></p>
-	<project-info>
+	<project-info> -->
+
 </project-card>
--- temp/modules/projects-database.php
+++ temp/modules/projects-database.php
@@ -3,6 +3,7 @@
 $projects_database = [
 	[	"thumbnail" => "modules/images/popcorn.jpg",
 		"title" => "Responsive Layout Popcorn Website",
+		"category" => "HTML, CSS",
 		"description" => "This is a site for fans of popcorn in all its deliciousness. It contains a header, a welcome section, some profile cards, and a footer",
 		"goal" => "The goal was to  create a website that looks good and is responsive on every viewport size",
 		"process" => "Used the flex box layout module and changed its property values at different viewport sizes using media queries",
@@ -11,6 +12,7 @@ $projects_database = [
 
 	[	"thumbnail" => "modules/images/tech-site.jpg",
 		"title" => "Tech Brochure Website",
+		"category" => "HTML, CSS",
 		"description" => "This is a brochure website for a life-style VR tech device. It's responsive at every viewport and contains buttons and links with hover states. ",
 		"goal" => "The goal was to style a website utilizing a prebuilt base code without changing the HTML. ",
 		"process" => "Using CSS and PHP (the base code is a collection of php modules) I  navigated through multiple files to style the basecode without touching the HTML. I kept track of all my changes by using comments and writing my code at the bottome of the main css document.",
@@ -19,6 +21,7 @@ $projects_database = [
 
 	[	"thumbnail" => "modules/images/exercises.jpg",
 		"title" => "Forms with PHP",
+		"category" => "HTML, PHP",
 		"description" => "This project is a collection of different forms generated using PHP. Some programs will create a madlib. Other forms will display mathmatical calculations. ",
 		"goal" => "The goal was to get practice with php by creating simple programs based on the exercises from Brian Hogan's Exercises for Programmers (2015)",
 		"process" => "I combined PHP and HTML to produce a form that on submission produces a series of outputs based on conditional php statements",
