fcaf3190699d96f8aa800ab7a35a92e44b8683b2
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-03-02 09:17:43 -0500

    put think dividers back in

--- temp/home.php
+++ temp/home.php
@@ -3,33 +3,33 @@
 			<inner-column>
 				<?php include("modules/introduction.php");?>
 			</inner-column>
-			<!-- <div></div> -->
+			<div></div>
 		</section>
 
 		<section id="about" class = "about">
 			<inner-column>
 				<?php include("modules/about.php");?>
 			</inner-column>
-			<!-- <div></div> -->
+			<div></div>
 		</section>
 		
 		<section id="writing" class = "writing">
 			<inner-column>
 				<?php include("modules/blog-intro.php");?>
 			</inner-column>
-			<!-- <div></div> -->
+			<div></div>
 		</section>
 		
 		<section id="projects" class = "projects">
 			<inner-column>
 				<?php include("modules/projects-intro.php");?>
 			</inner-column>
-			<!-- <div></div> -->
+			<div></div>
 		</section>
 		
 		<section id="playlist" class= "playlist">
 			<inner-column>
 				<?php include("modules/playlist-intro.php");?>
 			</inner-column>
-			<!-- <div></div> -->
+			<div></div>
 		</section>
