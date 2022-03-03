a39e81ca7c44fbead45454b736fb854e0a705258
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-03-03 14:48:06 -0500

    change goals page

--- goals/index.php
+++ goals/index.php
@@ -1,14 +1,29 @@
-<h1>Goals</h1>
-<h2>As of Feb 11th, 2022</h2>
+<h1> Web Developer Goals</h1>
+<h2>As of March 3rd, 2022</h2>
 
-<ul>
-	<li>To earn a job working in web development by the summer of 2022</li>
-	<li>To learn Javascript</li>
-	<li>To get better at markdown</li>
-	<li>To create and maintain a blog focused on web dev and hip hop </li>
-	<li>To become a better pixel art artist</li>
-	<li>Learn ways to incorporate my pixel art into my web development</li>
-	<li>To help build my newphew's website for his T-shirt business.</li>
-	<li>to graduate with my Ph.D. in May 2022</li>
-</ul>
+<div class="top-priority">
+	<h1>Highest Priority</h1>
+	<ul>
+		<li>To earn a job working in web development by the summer of 2022</li>
+	</ul>
+
+</div>
+
+
+<div class="high-priority">
+	<h1>High Priority</h1>
+	<ul>
+		<li>To maintain a blog focused on web dev and hip hop</li>
+		<li>To get better at creating and implimenting SVG graphics</li>
+		<li>To learn Javascript</li>
+	</ul>
+</div>
+
+<div class="medium-priority">
+	<h1>Medium Priority</h1>
+	<ul>
+		<li>To get better at markdown</li>
+	</ul>
+</div>
+
 
