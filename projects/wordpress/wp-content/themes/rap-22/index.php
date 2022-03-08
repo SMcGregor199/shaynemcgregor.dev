da141bde1fc41417dcb8ca6ffcef525d46db538d
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-03-08 14:50:23 -0500

    add home and list pages to project

--- projects/wordpress/wp-content/themes/rap-22/index.php
+++ projects/wordpress/wp-content/themes/rap-22/index.php
@@ -1,3 +1,26 @@
 <?php get_header(); ?>
-<h1>testing rap hall of fame</h1>
+
+	<?php 
+
+	if ( is_page('home') ) {
+		echo '<h1>home</h1>';
+	}
+
+	if ( is_page('list') ) {
+		    $args = array(  
+        'post_type' => 'rappers',
+    );
+
+    $loop = new WP_Query( $args ); 
+        
+    while ( $loop->have_posts() ) : $loop->the_post(); 
+        echo "<h2>" . get_the_title() . "</h2>"; 
+        the_excerpt(); 
+    endwhile;
+
+    wp_reset_postdata(); 
+	}
+
+	?>
+
 <?php get_footer(); ?>
