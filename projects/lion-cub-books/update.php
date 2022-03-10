6b0686af5a4718d6f5f1bc451f4c56ee58af0039
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-03-10 10:10:30 -0500

    I fixed the update function in lion books

--- projects/lion-cub-books/update.php
+++ projects/lion-cub-books/update.php
@@ -21,7 +21,7 @@
 	$title = $current_book["title"];
 	$author = $current_book['author'];
 	$blurb = $current_book["blurb"];
-	$id = $current_book['genre'];
+	$genre = $current_book['genre'];
 
 	
 
@@ -29,12 +29,13 @@
 	$hasTitle = false;
 	$hasAuthor = false;
 	$hasBlurb = false;
+	$hasGenre = false;
 	$titleError = null;
 	$authorError = null;
 	$blurbError = null;
 
 
-	$hasGenre = null;
+	
 
 
 	if($submitted) {
@@ -49,33 +50,33 @@
 			}
 		}
 
-		if( isset($_POST['author']) ){
-			$author = $_POST["author"];
+		// if( isset($_POST['author']) ){
+		// 	$author = $_POST["author"];
 			
-			if( strlen($author) > 0 ) {
-				$hasAuthor = true;
-			} else {
-				$authorError = "please add the name of the author";
-			}
-		}
+		// 	if( strlen($author) > 0 ) {
+		// 		$hasAuthor = true;
+		// 	} else {
+		// 		$authorError = "please add the name of the author";
+		// 	}
+		// }
 
-		if( isset($_POST['blurb']) ){
-			$blurb = $_POST["blurb"];
+		// if( isset($_POST['blurb']) ){
+		// 	$blurb = $_POST["blurb"];
 
-			if( strlen($blurb) > 0 ) {
-			//maybe change the string length on this? 
-				$hasBlurb = true; 
-			}  else {
-				$blurbError = "please add the blurb";
-			}
-		}
+		// 	if( strlen($blurb) > 0 ) {
+		// 	//maybe change the string length on this? 
+		// 		$hasBlurb = true; 
+		// 	}  else {
+		// 		$blurbError = "please add the blurb";
+		// 	}
+		// }
 
-		if( isset($_POST['genre']) ){
-			$genre = intval($_POST["genre"]);
-			$hasGenre = true;
-		} 
+		// if( isset($_POST['genre']) ){
+		// 	$genre = intval($_POST["genre"]);
+		// 	$hasGenre = true;
+		// } 
 
-		if($hasTitle && $hasAuthor && $hasBlurb && $hasGenre) {
+		if($hasTitle) {
 			
 			$book = [
 				
@@ -83,12 +84,12 @@
 				'author' => $author,
 				'blurb' => $blurb,
 				'genre' => $genre,
-				'book-cover' => $bookCover
+				'book-cover' => $bookCover,
 
 			];
 
 			$books = getDatabase();
-			$books[$current_book_id][$current_book] = $book;
+			$books[$current_book_id] = $book;
 			saveDatabase($books);
 		}
 		
