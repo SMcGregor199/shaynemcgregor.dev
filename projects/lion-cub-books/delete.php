bce16acbc8b5d954602bc5b515d0981856e8f970
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-03-10 10:00:43 -0500

    archived a project and worked on update

--- projects/lion-cub-books/delete.php
+++ projects/lion-cub-books/delete.php
@@ -1,18 +1,17 @@
 <?php 
 	
+	
 
-	$addedBooks_json = file_get_contents("addedBooks.json");
-	$database = json_decode($addedBooks_json,true);
+	$books=getDatabase();
 
 	$current_book_id = $_GET['book'];
 
 	// get the book's name associated with the id
 	// look through all books and see if it's Native Son
-	foreach ($database as $id => $book) {
+	foreach ($books as $id => $book) {
 		if($current_book_id == $id) {
-			unset($database[$current_book_id]);
-			$json = json_encode($database);
-			file_put_contents("addedBooks.json", $json);
+			unset($books[$current_book_id]);
+			saveDatabase($books);
 			$deletedBook=$book;
 		}
 	}
--- projects/lion-cub-books/update.php
+++ projects/lion-cub-books/update.php
@@ -1,28 +1,39 @@
 
 
 <?php
-	$book = getBookById($_GET['book']);
-	show($book);
 
+	$books = getDatabase();
+
+	$current_book_id = $_GET['book'];
+
+	$current_book = getBookById($current_book_id);
+	show($current_book);
+
+// This could be it's own getGenresDatabase function
+// The genre option in the form pulls from this database 
 	$genre_json = file_get_contents("genres.json");
 	$genres = json_decode($genre_json,true);
 
-	$database = getDatabase();
-
-	$title = $book["title"];
-	$author = $book['author'];
-	$blurb = $book["blurb"];
-	$id = $book['genre'];
 
 	$submitted = isset($_POST["submitted"]);
 
-	$bookCover = null;
+	// Initial values within the form 
+	$title = $current_book["title"];
+	$author = $current_book['author'];
+	$blurb = $current_book["blurb"];
+	$id = $current_book['genre'];
+
+	
+
+	$bookCover = "https://peprojects.dev/images/portrait.jpg";
 	$hasTitle = false;
 	$hasAuthor = false;
 	$hasBlurb = false;
 	$titleError = null;
 	$authorError = null;
 	$blurbError = null;
+
+
 	$hasGenre = null;
 
 
@@ -38,15 +49,47 @@
 			}
 		}
 
-		if($hasTitle) {
+		if( isset($_POST['author']) ){
+			$author = $_POST["author"];
+			
+			if( strlen($author) > 0 ) {
+				$hasAuthor = true;
+			} else {
+				$authorError = "please add the name of the author";
+			}
+		}
+
+		if( isset($_POST['blurb']) ){
+			$blurb = $_POST["blurb"];
+
+			if( strlen($blurb) > 0 ) {
+			//maybe change the string length on this? 
+				$hasBlurb = true; 
+			}  else {
+				$blurbError = "please add the blurb";
+			}
+		}
+
+		if( isset($_POST['genre']) ){
+			$genre = intval($_POST["genre"]);
+			$hasGenre = true;
+		} 
+
+		if($hasTitle && $hasAuthor && $hasBlurb && $hasGenre) {
+			
 			$book = [
 				
 				'title' => $title,
+				'author' => $author,
+				'blurb' => $blurb,
+				'genre' => $genre,
+				'book-cover' => $bookCover
+
 			];
 
-			$database[$id][$book] = $book;
-			$json = json_encode($database);
-			file_put_contents("addedBooks.json", $json);
+			$books = getDatabase();
+			$books[$current_book_id][$current_book] = $book;
+			saveDatabase($books);
 		}
 		
 	}
