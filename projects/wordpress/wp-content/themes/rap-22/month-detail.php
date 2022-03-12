1ff0ab539e144f61f646c3e4c1a1245ade79e002
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-03-12 18:09:20 -0500

    changes

--- projects/wordpress/wp-content/themes/rap-22/month-detail.php
+++ projects/wordpress/wp-content/themes/rap-22/month-detail.php
@@ -9,3 +9,26 @@ foreach ($album_list as $album) {
 
 ?>
 
+<section>
+	<inner-column>
+		<?php
+			$album_list = get_field('album_list');
+			if( $album_list ): 
+		?>
+				<ul>
+					<?php foreach( $album_list as $album_list ): 
+						$permalink = get_permalink( $album_list->ID );
+						$title = get_the_title( $album_list->ID );
+						$custom_field = get_field( 'field_name', $album_list->ID );
+					?>
+					<li>
+						<a href="<?php echo esc_url( $permalink ); ?>"><?php echo esc_html( $title ); ?></a>
+						<span>A custom field from this post: <?php echo esc_html( $custom_field ); ?></span>
+					</li>
+					<?php endforeach;
+					 ?>
+				</ul>
+			<?php endif;
+		?>
+	</inner-column>
+</section>
