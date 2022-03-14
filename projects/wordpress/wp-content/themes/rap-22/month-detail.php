0184db41d6ce0ddf30dd496eb5ab0503298ada75
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-03-14 14:13:28 -0400

    fix month detail page

--- projects/wordpress/wp-content/themes/rap-22/month-detail.php
+++ projects/wordpress/wp-content/themes/rap-22/month-detail.php
@@ -1,12 +1,5 @@
 <?php 
 $album_list = get_field('album_list');
-
-
-foreach ($album_list as $album) {
-	echo "This is an album";
-	// code...
-}
-
 ?>
 
 <section>
@@ -19,11 +12,11 @@ foreach ($album_list as $album) {
 					<?php foreach( $album_list as $album_list ): 
 						$permalink = get_permalink( $album_list->ID );
 						$title = get_the_title( $album_list->ID );
-						$custom_field = get_field( 'field_name', $album_list->ID );
+						$custom_field = get_field( 'artist_name', $album_list->ID );
 					?>
 					<li>
 						<a href="<?php echo esc_url( $permalink ); ?>"><?php echo esc_html( $title ); ?></a>
-						<span>A custom field from this post: <?php echo esc_html( $custom_field ); ?></span>
+						<span><?php echo esc_html( $custom_field ); ?></span>
 					</li>
 					<?php endforeach;
 					 ?>
