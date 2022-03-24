0cd7b82caaba885ea94eca7c22a25bba6662e28e
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-03-24 09:44:50 -0400

    create pages for rap blog site

--- projects/wordpress/wp-content/themes/rap-22/index.php
+++ projects/wordpress/wp-content/themes/rap-22/index.php
@@ -6,32 +6,23 @@
 		include("templates/pages/home.php");
 	}
 
-	if ( is_page('albums') ) {
-		$args = array(  
-			'post_type' => 'album',
-		);
-
-		$loop = new WP_Query( $args ); 
-
-		while ( $loop->have_posts() ) : $loop->the_post(); 
-			include('templates/components/album-card.php');
-		 
-		endwhile;
-
-		wp_reset_postdata(); 
+	if (is_page('blog') ) { 
+		
 	}
 
-	if ( is_singular('album') ) {
-		echo "<h1>This is the detail page</h1>";
+	if (is_page('album-voting') ) { 
+		
 	}
 
-	if (is_page('months') ) { //list page
-		include('months-list.php');
+	if (is_page('track-of-the-week') ) { 
+		
 	}
 
-	if (is_singular('month') ) { //detail page
-		include('month-detail.php');
+	if (is_page('album-of-the-month') ) { 
+	
 	}
+
+
 ?>
 
 <?php get_footer(); ?>
