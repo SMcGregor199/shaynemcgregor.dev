1a49810ee99cf8e5c7da58b2ac1ee22d4055f479
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-04-09 02:33:49 -0400

    add space at top of PHP garden

--- php-exercises/modules/masthead.php
+++ php-exercises/modules/masthead.php
@@ -1,5 +1,5 @@
 
 <mast-head>
 <h1 class="title-voice">PHP Exercises Layout Garden</h1>
-<p class="description-voice">This is where you can see all the mini-projects I've been creating with PHP. All the projects shown here come from Brian's <span>Exercises for Programmers: 57 Challenges to Develop Your Coding Skills</span>(2015)</p>
+<p class="description-voice">This is where you can see all the mini-projects I've been creating with PHP. All the projects shown here come from Brian's <span>Exercises for Programmers: 57 Challenges to Develop Your Coding Skills</span>&nbsp&nbsp&nbsp(2015)</p>
 </mast-head>
