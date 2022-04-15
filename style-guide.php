e2065f700a19ee1381688cd8c099c86a73252213
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-04-15 11:16:41 -0400

    styling the style guide

--- style-guide.php
+++ style-guide.php
@@ -1,17 +1,36 @@
 <?php include("modules/header.php");?>
 <main>
-	<h1 class="title-font">Style Guide</h1>
+	<h1 class="page-title title-font">Style Guide</h1>
 	<inner-column>
+
 	<h2 class="section-heading-font">Fonts</h2>
 	<hr>
 	<p class="paragraph-voice">Red Hat Display</p>
+
 	<h2 class="section-heading-font">Type Patterns</h2>
-	<p class="paragraph-voice">Different font sizes used throughout the site, classified into what I call "type patterns".</p>
 	<hr>
+	<div class="type-patterns">
+		<inner-column>
+			<h1 class="title-font">ABCDEFGHIJKLMNOPQRSTUVWXYZ</h1>
+			<h1 class="title-font">1234567890</h1>
+			<h2 class="section-heading-font">ABCDEFGHIJKLMNOPQRSTUVWXYZ</h2>
+			<h2 class="section-heading-font">1234567890</h2>
+			<p class="paragraph-voice">ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
+			<p class="paragraph-voice">1234567890</p>
+		</inner-column>
+	</div>
+
 	<h2 class="section-heading-font">Colors</h2>
 	<hr>
+
 	<h2 class="section-heading-font">Links</h2>
 	<hr>
+	<div class="links">
+		<inner-column>
+			<a href="#" class="link-font">Read</a>
+		</inner-column>
+	</div>
+
 	<h2 class="section-heading-font">Site-Menu</h2>
 	<hr>
 	
