52a041349e878843f519aff23858d8c81d596925
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-04-16 12:09:27 -0400

    added updated about section

--- modules/about.php
+++ modules/about.php
@@ -1,19 +1,6 @@
-<about>
-	<article>
-		<h1 class = "attention-voice">Who I Am</h1>
-
-		<p class = "paragraph-voice">I am a web designer and Ph.D. candidate at Yale University in African American Studies. These two aspects of who I am allow me to view the internet as a meidum of communication and think critically about how it can be leveraged in the project of creating a more equitable world.</p>
-	</article>
-
-	<article>
-		<h1 class = "attention-voice">What I Do</h1>
-
-		<p class = "paragraph-voice">I design and develop websites. I listen and reflect on rap music every day. I consistently write about both</p>
-	</article>
-
-	<article>
-		<h1 class = "attention-voice">Goals</h1>
-		
-		<p class = "paragraph-voice">I want to find a company where I can continue to expand my skillset as a developer and designer. I also want to write 50 blog posts by the end of the year.</p>
-	</article>
-</about>
+<section-heading>
+	<h2 class="section-heading-font">My Story</h2>
+	<p class="paragraph-voice">I am a web designer and  hold a Ph.D. in African American Studies and English from Yale University. Thinking about web design through the lens of race and literature, allows me to pursue questions related to user accessibility and legibility.</p>
+	<p class="paragraph-voice">I want to find a company where I can continue to expand my skillset as a developer and designer. Because writing is such a big part of who I am, I also want to write 50 blog posts by the end of the year. Click below to learn more about my specific goals and stay up-to-date on my progress.</p>
+	<a href="../goals" class="link-font">Goals</a>
+</section-heading>
