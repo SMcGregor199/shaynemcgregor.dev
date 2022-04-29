f585e94ac409fef84a6a5d01603463cc81e65b47
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-04-29 09:26:04 -0400

    include blogs json

--- pages/home.php
+++ pages/home.php
@@ -20,35 +20,9 @@
 		?>
 		<?php include("modules/section-heading.php"); ?>
 
-		<?php 
-
-			$blog1 = [
-				"title" => "How Power Rangers Taught me&nbsp;PHP",
-				"content" => "It's Morphing Time",
-				"link" => "https://shaynemcgregor.substack.com/p/how-the-power-rangers-taught-me-php",
-				"linkTarget" => "Power Rangers",
-				"linkText" => "Read",
-			];
-
-			$blog2 = [
-				"title" => "This is a Collaborative Environment",
-				"content" => "Are you ready to collab?",
-				"link" => "https://shaynemcgregor.substack.com/p/this-is-a-collaborative-environment",
-				"linkTarget" => "This is a Collaborative Environment",
-				"linkText" => "Read",
-			];
-
-			$blog3 = [
-				"title" => "Why I love J Cole's let.go.my.hand",
-				"content" => "J Cole's The Off Season",
-				"link" => "https://shaynemcgregor.substack.com/p/hip-hopwhy-i-love-j-coles-letgomyhand?s=w",
-				"linkTarget" => "Why I love J Cole's let.go.my.hand",
-				"linkText" => "Read",
-			];
-			?>
-		<?php $blogs = [$blog1, $blog2, $blog3]; ?>
-		<?php $blogsJson = json_encode($blogs);
-		file_put_contents ('data/blogs.json', $blogsJson);	?>
+		<?php $blogsJson = file_get_contents('data/blogs.json');
+				$blogs = json_decode($blogsJson, true);
+		 ?>
 		<?php include("modules/blog-gallery.php") ?>
 	</inner-column>
 	<space></space>
@@ -80,21 +54,8 @@
 		<?php include("modules/section-heading.php"); ?>
 
 		<?php
-
-			$phpGarden = [
-				"title" => "PHP Garden",
-				"content" => "PHP is a server-size scripting language that's important for stitching files together and creating forms that receive user input. Click below to see the cool forms I've been creating with PHP.",
-				"link" => "?page=php-exercises",
-				"linkText" => "Click Here",
-				];
-			$layoutGarden = [
-				"title" => "Layout Garden",
-				"content" => "Positioning elements on a webpage is a critical part of making webpages readable. Click below to check out my cool responsive layout skills",
-				"link" => "../responsive-layout-garden",
-				"linkText" => "Click Here",
-			];
-
-			$gardens = [$phpGarden, $layoutGarden];
+			$gardensJson = file_get_contents("data/gardens.json");
+			$gardens = json_decode($gardensJson, true);
 		?>
 
 		<?php	include("modules/garden-gallery.php"); ?>	 
