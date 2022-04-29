d59d2643c81f89abba4fcec9b98f4bf10221a4e6
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-04-29 11:39:14 -0400

    update case study

--- pages/case-study.php
+++ pages/case-study.php
@@ -1,1 +1,23 @@
-<h1>This is a Case Study Page</h1>
+<?php 
+	$projectsJson = file_get_contents("data/projects.json");
+	$projects = json_decode($projectsJson, true);
+?>
+<?php 
+	$current_project_id = $_GET["projectID"]; 
+?>
+<?php foreach($projects as $projectID => $project) { ?>
+	<?php if($current_project_id == $projectID) { ?>
+
+		<h1>This is Lion Cub Case Study Page</h1>
+
+	<?php } else { ?>
+				<h1>This the Case Study of a different project</h1>
+				<?php }?>
+<?php } ?>
+
+
+
+
+
+			
+
