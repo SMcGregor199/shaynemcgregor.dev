08a1e629bcaa79e2c133f38c6b5d99a52855d7a2
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-05-02 09:56:47 -0400

    complete goals page

--- modules/list-module.php
+++ modules/list-module.php
@@ -1,6 +1,8 @@
 <list-card>
-	<h2 class="section-heading-font"><?=$goals['goalTitle']?></h2>
+	<h2 class="section-heading-font"><?=$section['listTitle']?></h2>
 	<ul class="paragraph-font">
+	<?php foreach($section["goalItems"] as $goalItem){ ?>
 		<li><?=$goalItem?></li>
+	<?php } ?>
 	</ul>
 </list-card>
--- pages/goals.php
+++ pages/goals.php
@@ -1,11 +1,19 @@
-
 <?php 
-	$goals['goalTitle'] = "Highest Priority";
-	$goalItem = "To work as a web developer";
+	$goalsPageJson = file_get_contents('data/goals.json');
+	$goalsPage = json_decode($goalsPageJson, true);
+	$sections = $goalsPage['sections'];
+ ?>
+
+<?php
+	foreach($sections as $section) { ?>
+	<section id="<?=$section['slug']?>" class="<?=$section['slug']?>">
+		<inner-column>
+			<?php include('modules/' . $section['module']); ?>	
+		</inner-column>
+		<space></space>
+	</section>
+	<?php } ?>
 ?>
-<?php include('modules/list-module.php'); ?>
-
-
 
 
 	
