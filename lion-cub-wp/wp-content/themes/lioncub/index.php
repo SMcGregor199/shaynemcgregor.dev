a992bd621fda92a8c806d6f5888262b2d903fc3b
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-05-13 16:04:08 -0400

    Add two example pages

--- lion-cub-wp/wp-content/themes/lioncub/index.php
+++ lion-cub-wp/wp-content/themes/lioncub/index.php
@@ -5,6 +5,14 @@
 	if ( is_page('home') ) {
 		include("templates/pages/home.php");
 	}
+
+	if ( is_page('example-page') ) {
+		include("templates/pages/example-page.php");
+	}
+
+	if ( is_page('example-page-2') ) {
+		include("templates/pages/example-page-two.php");
+	}
 ?>
 
 <?php get_footer(); ?>
