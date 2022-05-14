050a0d9f2f08e1fab94773e170bd449317280b0f
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-05-14 07:51:33 -0400

    Edit

--- modules/single-link.php
+++ modules/single-link.php
@@ -2,7 +2,7 @@
   $link = isset($section['moduleLink']) ? $section['moduleLink'] : "#";
   $linkText = isset($section['moduleLinkText']) ? $section['moduleLinkText'] : "Link";
  ?>
- 
+
 <nav>
 	<a href="<?=$link?>" class="link-font" rel="toggle"><?=$linkText?></a>
 </nav>
