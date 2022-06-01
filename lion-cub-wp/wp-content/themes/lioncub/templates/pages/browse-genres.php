2fde9533350f449d16a92185396fc904539ce203
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-06-01 12:46:40 -0400

    Get images working

--- lion-cub-wp/wp-content/themes/lioncub/templates/pages/browse-genres.php
+++ lion-cub-wp/wp-content/themes/lioncub/templates/pages/browse-genres.php
@@ -1,6 +1,7 @@
 <h2 class="section-heading-font">Genre Categories</h2>
 
 <?php
+	var_dump(get_field('book_cover'));
 	$parameters = array(
 	  "post_type" => "book",
 	  "orderby" => "ID",
@@ -12,7 +13,7 @@
 
 	while ($query->have_posts()) : $query->the_post();
 		$book_cover = get_field('book_cover')['url'];
-	   echo "<picture><img src='<?=$book_cover?>'></picture>";
+	   echo "<picture><img src='$book_cover'></picture>";
 	endwhile;
 
 	//reset what was done above.
