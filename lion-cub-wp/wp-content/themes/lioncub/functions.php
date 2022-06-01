8a0633def80032d02230d9c681958dc302524eb6
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-06-01 16:16:08 -0400

    Update to functions file

--- lion-cub-wp/wp-content/themes/lioncub/functions.php
+++ lion-cub-wp/wp-content/themes/lioncub/functions.php
@@ -20,4 +20,51 @@ function getFile($path) {
   return dirname(__FILE__) . '/' . $path;
 }
 
-?>
+//loops through custom taxonomy 
+function createGenreLinks($customTax, $postType) {
+  $genres = get_terms(
+                array($customTax),
+                array(
+                        'hide_empty'    => false,
+                        'orderby'       => 'name',
+                        'order'         => 'ASC',
+                        'number'        => 6 //specify yours
+                    )
+            );
+
+if( $genres ) :
+
+    foreach( $genres as $term ) :
+
+        //var_dump( $term );
+        echo "<a href=''>$term->name</a>";
+
+        $args = array(
+                'post_type'             => $postType,
+                'posts_per_page'        => 10, //specify yours
+                'post_status'           => 'publish',
+                'tax_query'             => array(
+                                            array(
+                                                'taxonomy' => 'category',
+                                                'field'    => 'slug',
+                                                'terms'    => $term->slug,
+                                            ),
+                                        ),
+                'ignore_sticky_posts'   => true //caller_get_posts is deprecated since 3.1
+            );
+        $_posts = new WP_Query( $args );
+
+        if( $_posts->have_posts() ) :
+            while( $_posts->have_posts() ) : $_posts->the_post();
+
+                echo '<h5>'. get_the_title() .'</h5>';
+
+            endwhile;
+        endif;
+        wp_reset_postdata(); //important
+
+    endforeach;
+
+endif; 
+}
+
--- lion-cub-wp/wp-content/themes/lioncub/templates/pages/browse-genres.php
+++ lion-cub-wp/wp-content/themes/lioncub/templates/pages/browse-genres.php
@@ -1,50 +1,8 @@
 <h2 class="section-heading-font">Genre Categories</h2>
 
 <?php
-$genres = get_terms(
-                array('genre'),
-                array(
-                        'hide_empty'    => false,
-                        'orderby'       => 'name',
-                        'order'         => 'ASC',
-                        'number'        => 6 //specify yours
-                    )
-            );
-
-if( $genres ) :
-
-    foreach( $genres as $term ) :
-
-        //var_dump( $term );
-        echo "<a href=''>$term->name</a>";
-
-        $args = array(
-                'post_type'             => 'book',
-                'posts_per_page'        => 10, //specify yours
-                'post_status'           => 'publish',
-                'tax_query'             => array(
-                                            array(
-                                                'taxonomy' => 'category',
-                                                'field'    => 'slug',
-                                                'terms'    => $term->slug,
-                                            ),
-                                        ),
-                'ignore_sticky_posts'   => true //caller_get_posts is deprecated since 3.1
-            );
-        $_posts = new WP_Query( $args );
-
-        if( $_posts->have_posts() ) :
-            while( $_posts->have_posts() ) : $_posts->the_post();
-
-                echo '<h5>'. get_the_title() .'</h5>';
-
-            endwhile;
-        endif;
-        wp_reset_postdata(); //important
-
-    endforeach;
-
-endif; ?>
+	createGenreLinks('genre','book');
+?>
 
 <?php
 	$parameters = array(
@@ -58,6 +16,9 @@ endif; ?>
 
 	while ($query->have_posts()) : $query->the_post(); ?>
 	
+	<?php
+	echo get_field('title'); 
+	?>
 		
 
 <?php	endwhile;
