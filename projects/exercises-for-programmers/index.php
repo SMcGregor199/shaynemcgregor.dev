7b04f999d3774b3a1818870d15458e1332b2ac92
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-07-11 15:58:09 -0400

    moved module logic to module file

--- projects/exercises-for-programmers/index.php
+++ projects/exercises-for-programmers/index.php
@@ -10,11 +10,6 @@
 <inner-column>
 	<?php 
 		foreach ($exerciseData['exercise-lists'] as $list) { ?>
-			<?php $listTitle = $list["language-name"]; 
-					foreach($list["exercise-names"] as $exerciseName => $status) {
-						
-					}
-			?>
 			<?php include("modules/list-w-emoji-circles.php"); ?>
 	<?php	} ?>
 </inner-column>
