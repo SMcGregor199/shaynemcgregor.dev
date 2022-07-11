427af2e4e91db3265fa375307e3ddb89d43f83b9
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-07-11 16:29:58 -0400

    Add Module Creator Zone

---
+++ projects/module-creator-zone/footer.php
@@ -0,0 +1,21 @@
+
+			<!-- STUFF -->
+		</main>
+
+
+		<footer class='site-footer'>
+		<inner-column>
+
+			<!-- no headers in footers allowed? -->
+			<div class='what-to-call-it-then'>
+				<h2 class='attention-voice'>This is the Module Creator Zone.</h2>
+
+			</div>
+
+			
+
+		</inner-column>
+		</footer>
+
+	</body>
+</html>
---
+++ projects/module-creator-zone/index.php
@@ -0,0 +1,18 @@
+
+
+<?php include('header.php'); ?>
+
+
+
+<section>
+<inner-column>
+
+
+
+</inner-column>
+</section>
+
+
+
+
+<?php include('footer.php'); ?>
