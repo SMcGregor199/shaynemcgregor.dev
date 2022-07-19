237a344e8e4d40c969b15a442fb15b909b99cffe
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-07-19 08:34:58 -0400

    add roundup module

---
+++ projects/create-a-module/components/roundup-card.svelte
@@ -0,0 +1,80 @@
+<section>
+	<inner-column>
+
+		<roundup-card>
+			<div class="roundup-intro">
+				<h2>Immigration</h2>
+				<p>My father, who I didn't know, returned to Kenya when I was just two. This country is more decent than one where a woman in Ohio, on the brink of retirement, finds herself one illness away from disaster after a lifetime of hard work. Passions fly on immigration, but I don't know anyone who benefits when a mother is separated from her infant child or an <a href="?">employer undercuts</a> American wages by hiring illegal workers.</p>
+			</div>
+
+			<ul class="roundup-list">
+				<li>
+					<span><a href="?">Immigration from the Caribbean</a></span>
+					<p class="description">He does not say that he was there because of Barack Obama. It's been about you. So let me be clear: no system of government can or should be imposed upon one nation by any other.</p>
+				</li>
+				<li>
+					<span><a href="?">Immigration from Kenya</a></span>
+					<p class="description">He does not say that he was there because of Barack Obama. It's been about you. So let me be clear: no system of government can or should be imposed upon one nation by any other.</p>
+				</li>
+				<li>
+					<span><a href="?">Immigration from the Global South</a></span>
+					<p class="description">He does not say that he was there because of Barack Obama. It's been about you. So let me be clear: no system of government can or should be imposed upon one nation by any other.</p>
+				</li>
+			</ul>
+
+			<blockquote cite="www.https://obamaipsum.com/">
+	        <p>"While studying here, my father met my mother. Our Party has chosen a man to lead us who embodies the best this country has to offer. And it's around this time that some pastors I was working with came up to me and asked if I was a member of a church. So doing the Lord's work is a thread that's run through our politics since the very beginning. We will all have to make concessions to achieve this. No, the word is very near."</p>	
+			</blockquote>
+		</roundup-card>
+
+	</inner-column>
+</section>
+
+
+<style>
+	roundup-card {
+		display: block;
+		display: flex;
+		flex-direction: column;
+		gap: 15px;
+	}
+
+	roundup-card p {
+		font-size: 16px;
+		line-height: 1.5;	
+		max-width: 64ch;
+	}
+
+	.roundup-intro h2 {
+		font-size: 36px;
+		font-weight: 400;
+	}
+
+	ul {
+		display: flex;
+		flex-direction: column;
+		gap: 15px;
+		list-style-type: disc;
+		margin-left: 16px;
+	}
+	ul li {
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+	}
+
+	ul a {
+		font-size: 20px;
+		text-decoration: underline;
+	}
+
+
+
+	blockquote {
+		border-width: 0;
+    	border-style: solid;
+    	border-left-width: 4px;
+    	padding-left: 30px;
+	}
+
+</style>
--- projects/create-a-module/src/routes/index.svelte
+++ projects/create-a-module/src/routes/index.svelte
@@ -1,6 +1,9 @@
 <script>
 	import Card from "../../components/card.svelte";
+	import Roundup from "../../components/roundup-card.svelte";
 </script>
 
-<Card />
+<!-- <Card /> -->
+<Roundup />
+
 
