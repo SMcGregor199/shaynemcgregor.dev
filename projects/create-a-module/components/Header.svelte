33e23d22127c425434518e2ed146841c215869e0
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-07-18 22:49:48 -0400

    add style settings to svelte code base

---
+++ projects/create-a-module/components/Header.svelte
@@ -0,0 +1,14 @@
+<header class='site-header'>
+	<inner-column>
+
+		<h1 class="attention-voice">Let's Create A Module</h1>
+
+	</inner-column>
+</header>
+
+<style>
+	.site-header {
+	  background-color: var(--color);
+	  color: var(--white);
+	}
+</style>
--- projects/create-a-module/src/routes/__layout.svelte
+++ projects/create-a-module/src/routes/__layout.svelte
@@ -1,6 +1,13 @@
 <script>
-	
+	import Reset from "../../css/meyer-reset.css";
+	import Setup from "../../css/setup.css";
+	import Settings from "../../css/settings.css"
+	import Structure from "../../css/structure.css"
+	import Typography from "../../css/typography.css"
+	import Header from '../../components/Header.svelte';
+	import Footer from '../../components/Footer.svelte';
 </script>
+
 
 <Header />
 
@@ -10,9 +17,7 @@
 
 <Footer />
 
-<style global>
 
-</style>
 
 
 
