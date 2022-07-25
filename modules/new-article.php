3987bafafde7b99b9841e1715b0981ae87c2fa42
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-07-25 17:43:36 -0400

    Add tailwind. Not a fan

--- modules/new-article.php
+++ modules/new-article.php
@@ -2,13 +2,13 @@
 	<inner-column>
 		<div class="new-about-wrapper">
 			<h2 class="new-about-title test">
-			<span>01</span> <span>About</span>
+			<span>01.</span> <span>About</span>
 			</h2>
 
 			<div class="new-about-text">
 				<p>Hi there! I'm Shayne, a front-end web developer.</p>
-				<p class="italic-text">But what I will not do is suggest that the Senator takes his positions for political purposes. Instead, it is that American spirit - that American promise - that pushes us forward even when the path is uncertain</p>
-				<p>Why associate myself with Reverend Wright in the first place, they may ask? Why not join another church?</p>
+				<p class="italic-text">I was raised by hip-hop and internet culture. Those two things instilled in me a love for websites and an desire to make them cool and funky</p>
+				<p>Today I'm enthralled by web development, creating websites that </p>
 				<p>I work with <b>WordPress</b>, <b>Svelte</b>, <b>JavaScript</b>, <b>PHP</b>, and <b>Tailwind</b>, to deliver quality front-end products.</p>
 			</div>
 		</div>
