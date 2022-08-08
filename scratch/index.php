bf1213925d1a7f0e2e7bb5e874ed5a273260886c
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-08-08 14:34:33 -0400

    js practice

--- scratch/index.php
+++ scratch/index.php
@@ -8,12 +8,11 @@
 </head>
 <body>
 	<header>
-		<?php
+<!-- 		<?php
 			$data = file_get_contents("https://pokeapi.co/api/v2/pokemon/pikachu");
 			$pokeData = json_decode($data, true);
-
 			print_r($pokeData);
-		?>
+		?> -->
 	</header>
 
 	<main>
@@ -24,9 +23,15 @@
 
 	<footer>
 		<script>
-				fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-				.then((response) => response.json())
-  				.then((json) => console.log(json));
+				// fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+				// .then((response) => response.json())
+  		// 		.then((json) => console.log(json));
+  		var myArray = [1,2,3,4,5,6,7,8,9,];
+  		var output = document.querySelector(".output");
+
+  		myArray.find(function(value){
+  			
+  		})
 		</script>
 	</footer>
 </body>
