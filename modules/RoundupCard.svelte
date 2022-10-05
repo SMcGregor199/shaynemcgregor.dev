51b755d6abac3da3b506cf35e43fb5e6999f90d5
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-10-05 15:46:31 -0400

    add roundup card module

---
+++ modules/RoundupCard.svelte
@@ -0,0 +1,28 @@
+
+<a href="2022/january/january-01" class="roundup-card">
+		<div class="roundup-title">
+			<p>January</p>
+			<h2>01</h2>
+		</div>
+
+		<div class="roundup-date">
+			<p>Weekly Roundup</p>
+			<time datetime='01.01.2022'>01.01.2022</time>
+		</div>
+</a>
+
+
+<style>
+	.roundup-card {
+		@apply block bg-beige-light w-fit border-solid border-4 border-black rounded-2xl flex flex-col gap-4 text-black p-4;
+	}
+	.roundup-title h2 {
+		@apply mb-0 font-[acuta] font-light;
+	}
+	.roundup-card p, time {
+		@apply mb-0 font-[halyard];
+	}
+	.roundup-title {
+		@apply flex justify-around items-center gap-[9rem];
+	}
+</style>
