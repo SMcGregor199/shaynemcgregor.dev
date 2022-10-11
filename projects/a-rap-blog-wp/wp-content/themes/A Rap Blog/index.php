34c11850a8a786de0749644beeebb93b2074f0dc
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-10-11 15:06:25 -0400

    add initial files for rap blog

---
+++ projects/a-rap-blog-wp/wp-content/themes/A Rap Blog/index.php
@@ -0,0 +1,10 @@
+
+<?php get_header(); ?>
+
+<?php 
+	if ( is_page('home') ) {
+		include("templates/pages/home.php");
+	}
+?>
+
+<?php get_footer(); ?>
---
+++ projects/a-rap-blog-wp/wp-content/themes/A Rap Blog/templates/pages/home.php
@@ -0,0 +1,5 @@
+<section>
+	<inner-column>
+		<h1>This is the home page</h1>
+	</inner-column>
+</section>
