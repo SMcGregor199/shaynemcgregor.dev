2ea0720fb10dcbacb08dda9f18a11d31bce120d7
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-10-25 11:31:12 -0400

    add author’s post page

--- projects/a-rap-blog-wp/wp-content/themes/A Rap Blog/blocks/blog-header/blog-header.php
+++ projects/a-rap-blog-wp/wp-content/themes/A Rap Blog/blocks/blog-header/blog-header.php
@@ -4,6 +4,6 @@ $subTitle = get_field('sub-title');
 <div class="blog-header">
 	<h3><?=$subTitle?></h3>
 	<div class="date-and-attribution">
-		<time datetime="#"><?=get_the_date("F j, Y");?></time><p>by<a href="#"><?php the_author(); ?></a></p>
+		<time datetime="#"><?=get_the_date("F j, Y");?></time><p>by<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>"><?php the_author(); ?></a></p>
 	</div>
 </div>
