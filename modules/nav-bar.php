8035cc7df30d6fb37698cf8cb045d48a577e2d66
SMcGregor199 <shaynemcgregor1@gmail.com>
2023-01-10 06:03:58 -0500

    created new header

--- modules/nav-bar.php
+++ modules/nav-bar.php
@@ -1,9 +1,11 @@
 <header class="home-header">
 	<inner-column>
-		<nav class="link-voice site-nav">
-			<a href="?page=home">Home</a>
-			<a href="#">Blogging</a>
-			<a href="#">Project Archive</a>	
+		<nav class="site-nav">
+			<a class="link-voice" href="?page=home">Home</a>
+			<span>/</span>
+			<a class="link-voice" href="#">Blogging</a>
+			<span>/</span>
+			<a class="link-voice" href="#">Project Archive</a>	
 		</nav>
 	</inner-column>
 </header>
--- projects/js-quiz-app/app.js
+++ projects/js-quiz-app/app.js
@@ -1,26 +1,42 @@
-var games = [];
 
-function update(){
+var games = [];
+var gamesIdMaker = 0;
+
+function print(note = ""){
+    console.log(`-----------${note}`);
     console.log(`games`, games);
 }
 
 function add(content){
-    var game = {
-        content: content
+    const game = {
+        id:`a${gamesIdMaker++}`,
+        title: content
     }
     games.push(game);
-    update();
+    print(`Added ${content}`);
 }
 
 function remove(id){
+    print(`Removed ${games[id].title}`);
     games.splice(id,1);
-    update();
 }
 
-add("God of War");
+function complete(id){
+    games[id].complete = true;
+    print(`Completed ${games[id].title}`);
+}
+function udpate(id,property,value){
+    games[id][property] = value;
+    print(`Updated the ${property} of ${games[id].title}`);
+}
+
+add('God of War');
 add('Uno');
-add('Monopoly');
-remove(1);
+add(`Monopoly`);
+
+udpate(0, "title", "Super Mario");
+udpate(1, "id", "b57");
+
 
 
 
