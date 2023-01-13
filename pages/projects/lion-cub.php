bc12c5fe9f600a588d642a0f156bfd28a29272a1
SMcGregor199 <shaynemcgregor1@gmail.com>
2023-01-13 06:59:53 -0500

    added lion svg to lion cub projects page

--- pages/projects/lion-cub.php
+++ pages/projects/lion-cub.php
@@ -3,19 +3,26 @@
 
 <section>
 	<inner-column>
-
+	<h1 class="super-title-voice">Lion Cub Books</h1>
 	</inner-column>
 </section>
 
 <section>
 	<inner-column>
-	
+	<p>And yet, I know that, on this night, they look down on me with great pride. But we also know that government initiatives are not enough. Ironically, this quintessentially American - and yes, conservative - notion of self-help found frequent expression in Reverend Wright's sermons. With profound gratitude and great humility, I accept your nomination for the presidency of the United States. That promise is our greatest inheritance. And I am hopeful that all countries in the region can share in this goal.</p>
+	<picture>
+		<?php include('lion.svg'); ?>
+	</picture>
 	</inner-column>
 </section>
 
 <section>
 	<inner-column>
-	
+		<div class="blog-links">
+			<a href="https://shaynemcgregor.substack.com/p/the-future-of-lion-cub-books">The Future of Lion Cub Books</a>
+		<a href="#">Using Lion Cub Books to Learn ACF Pro Concepts</a>
+		</div>
+		<div class="project-link"><a href="#">Lion Cub Books Homepage</a></div>
 	</inner-column>
 </section>
 
