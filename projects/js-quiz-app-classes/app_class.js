bba861e8db924f1f74b31597f29cf85a50fd0508
SMcGregor199 <shaynemcgregor1@gmail.com>
2023-01-16 05:44:53 -0500

    rearrange JS Classes file for readability

--- projects/js-quiz-app-classes/app_class.js
+++ projects/js-quiz-app-classes/app_class.js
@@ -13,23 +13,36 @@ class GameListClass {
     add(content){
         const game = {
             id:`a${this.gamesIDMaker++}`,
-            title: content
+            title: content,
+            complete: false
         };
         this.games = [...this.games,game];
+    }
+    remove(id){
+        const filtered = this.games.filter(function(game){
+            return game.id != id;
+        })
+        this.games = [...filtered];
+        renderGames(this.games);
+        //splice is a good method to use if you're using the position of
+        //what's in the array. But we're using the id of the game inside the 
+        //game object
+        // this.games.splice(id,1);
     }
     update(id,property,value){
         this.games[id][property] = value;
     }
     complete(id){
         this.games[id].complete = true;
-    }
-    remove(id){
-        this.games.splice(id,1);
-    }
-    
+    }   
 }
 
-// render the data
+//defining the list 
+const playStationList = new GameListClass('PlayStation Games');
+
+
+
+// Defining the functions that will render the data
 function renderGame(game){
     return `
         <li data-id="${game.id}">
@@ -37,6 +50,7 @@ function renderGame(game){
                 <h2>${game.title}</h2>
                     <actions>
                         <button>Remove</button>
+                        <button>Complete</button>
                     </actions>
             </game-card>
         </li>`
@@ -54,18 +68,21 @@ function renderGames(games) {
     $output.innerHTML = template;
 }
 
-// Form functions 
+
+
+// Form functions. The thing to keep in mind is that while it works, this 
+// form is rebuilding the list EACH time you press submit, as opposed to "reactively"
+// which would be possible with something like Vue or SvelteKit 
 $form.addEventListener('submit',function(event){
     event.preventDefault();
-    const userList = new GameListClass('PlayStation Games');
-    userList.add($input.value);
-    renderGames(userList.games);
+    playStationList.add($input.value);
+    renderGames(playStationList.games);
     $input.value = "";
 })
 
 $output.addEventListener('click',function(event){
     if(event.target.textContent == 'Remove'){
         const id = event.target.closest('li').dataset.id;
-        remove(id);
+        playStationList.remove(id);
     }
 })
