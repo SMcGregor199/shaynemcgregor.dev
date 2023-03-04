cc9b8ba6052188a028b1edd6c9b7fdd316a98ed3
SMcGregor199 <shaynemcgregor1@gmail.com>
2023-03-04 17:05:20 -0500

    remove lion-cub-wp from personal website

--- projects/lion-cub-wp/wp-content/themes/lioncub/templates/components/nav-list.php
+++
@@ -1,8 +0,0 @@
-<h2 class="section-heading-font"><?=$navTitle?></h2>
-<nav class="nav-list">
-	<ul class="link-font">
-	<?php foreach($navItems as $navItem) { ?>
-		<li><a href="<?=$navItem['navLink']?>"><?=$navItem['navText']?></a></li>
-	<?php } ?>
-	</ul>
-</nav>
--- projects/lion-cub-wp/wp-content/themes/lioncub/to-change.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-
-// List of things to change from default WordPress Codebase 
-
-//1. Change content attribute in the descritpion meta tag in head element 
-//2. Change content attribute in the property meta tag in head element 
-//3. Change meta deta info in the commented section of the style css file
