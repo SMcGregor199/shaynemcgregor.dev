176af42a927811289cd6a08cd7e41bc3ece6ccdc
SMcGregor199 <shaynemcgregor1@gmail.com>
2023-04-16 11:55:21 -0400

    add hero section

---
+++ modules/hero.php
@@ -0,0 +1,13 @@
+
+<div class="hero">
+    <h1 class="major-voice">Shayne McGregor</h1>
+    <p class="minor-voice">The Hip Hop Web Developer</p>
+    <div class="button-group">
+        <div class="minor-voice button" style="color: var(--white);background-color:var(--accent)">
+            <a href="">View Projects</a>
+        </div>
+        <div class="minor-voice button" style="color: var(--accent); background-color:var(--accent-mid)">
+            <a href="">Contact Me</a>
+        </div>
+    </div>
+</div>
