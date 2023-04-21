caf495f65872e7d369ca809d5202077fe4524c02
SMcGregor199 <shaynemcgregor1@gmail.com>
2023-04-20 20:37:25 -0400

    update projects link to GitHub Repo

--- modules/site-header.php
+++ modules/site-header.php
@@ -7,7 +7,7 @@
 			<nav class="site-nav">
 				<a class="base-voice" href="?page=home"><span>home</span></a>
 				<a class="base-voice" href="#"><span>articles</span></a>
-				<a class="base-voice" href="#"><span>projects</span></a>
+				<a class="base-voice" target="new_window" href="https://github.com/SMcGregor199/personal-website"><span>projects</span></a>
 				<a class="base-voice" target="new_window" href="https://www.linkedin.com/in/shayne-mcgregor/"><span>contact</span></a>	
 			</nav>
 		</div>
