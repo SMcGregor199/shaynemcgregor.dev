8a68e579acbd77095d5220d58a4366605efb1bf1
SMcGregor199 <shaynemcgregor1@gmail.com>
2023-04-22 16:09:32 -0400

    add articles section to project grid

--- modules/project-article-grid.php
+++ modules/project-article-grid.php
@@ -1,10 +1,49 @@
 
+<div class="container">
 
-<div>
-    <h2 class="base-voice">Project: Flagship WordPress Project</h2>
-   <a href="#">
-        <h2 class="base-voice">Click bait title</h2>
-    </a> 
+
+    <div style="max-width:50%; display:flex; flex-direction:column; gap:30px;">
+        <h2 class="base-voice">Project: Flagship WordPress Project</h2>
+        <a href="#" style="border:1px solid var(--outlines); border-radius:15px;">
+                <picture class="graphic" style="padding:25px"; >
+                    <?php include('images/svgs/lion.svg'); ?>
+                </picture>
+                <h2 class="base-voice" style="padding-left:43px; padding-right:43px; padding-bottom:10px;" >This is click bait title for your projects! Click here. Click</h2>
+        </a> 
+    </div>
+
+    <div style="max-width:50%; display:flex; flex-direction:column; gap:30px;">
+        <h2 class="base-voice">Articles</h2>
+        <div>
+            <a href="#" style="border:1px solid var(--outlines); border-radius:15px;">
+            
+            <h2 class="base-voice">
+              This is click bait title for your projects! Click here. Click
+            </h2>
+            
+            </a>
+            <a href="#" style="border:1px solid var(--outlines); border-radius:15px;">
+            
+            <h2 class="base-voice">
+              This is click bait title for your projects! Click here. Click
+            </h2>
+            
+            </a>
+            <a href="#" style="border:1px solid var(--outlines); border-radius:15px;">
+                
+                <h2 class="base-voice">
+                This is click bait title for your projects! Click here. Click
+                </h2>
+                
+            </a>
+            <a href="#" style="border:1px solid var(--outlines); border-radius:15px;">
+                
+                <h2 class="base-voice">
+                This is click bait title for your projects! Click here. Click
+                </h2>
+                
+            </a>
+        </div>
+    </div>
+
 </div>
-
-
