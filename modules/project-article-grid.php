aaff5fdb7ccd009388b8fceff3c000f61033a53c
Henry_DelGlitch <CodingWHenry@gmail.com>
2023-04-23 12:56:38 -0400

    Added articles-column class to anchor elements, added CSS styling to said column. Styling for SVG added.

--- modules/project-article-grid.php
+++ modules/project-article-grid.php
@@ -1,56 +1,45 @@
-
 <div class="container">
-
-
     <div class="flag-ship-project" style="display:flex; flex-direction:column; gap:30px;">
         <h2 class="base-voice">Project: Flagship WordPress Project</h2>
-        <a href="#">
-                <picture class="graphic" style="padding:25px"; >
-                    <?php include('images/svgs/lion.svg'); ?>
-                </picture>
-                <h2 class="base-voice" style="padding-left:43px; padding-right:43px; padding-bottom:10px;" >This is click bait title for your projects! Click here. Click</h2>
-        </a> 
+            <a class="articles-column" href="#">
+                    <picture class="graphic" >
+                        <?php include('F:\xampp\htdocs\personal-website\images\svgs\lion.svg'); ?>
+                    </picture>
+                    <h2 class="base-voice" style="padding-left:43px; padding-right:43px; padding-bottom:10px;" >This is click bait title for your projects! Click here. Click</h2>
+            </a> 
     </div>
 
     <div class="articles" style="display:flex; flex-direction:column; gap:30px;">
         <h2 class="base-voice">Articles</h2>
-        <div class="articles-grid" style="gap:20px;">
-            <a href="#">
-            
-            <h2 class="base-voice" style="padding:25px";>
-              This is click bait title for your projects! Click here. Click
-            </h2>
-            <div class="border"></div>
-            
-            
+        <div class="articles-grid">
+            <a class="articles-column" href="#">
+                <h2 class="base-voice" style="padding:25px";>
+                This is click bait title for your projects! Click here. Click
+                </h2>
+                <div class="border"></div>
             </a>
-            <a href="#">
-            
-            <h2 class="base-voice" style="padding:25px";>
-              This is click bait title for your projects! Click here. Click
-            </h2>
-            <div class="border"></div>
-            
-            
+
+            <a class="articles-column" href="#">
+                <h2 class="base-voice" style="padding:25px";>
+                This is click bait title for your projects! Click here. Click
+                </h2>
+                <div class="border"></div>
             </a>
-            <a href="#">
-                
+
+            <a class="articles-column" href="#">
                 <h2 class="base-voice" style="padding:25px";>
                 This is click bait title for your projects! Click here. Click    
                 </h2>
                 <div class="border"></div>
-                
-                
             </a>
-            <a href="#">
-                
+
+            <a class="articles-column" href="#">
                 <h2 class="base-voice" style="padding:25px";>
                 This is click bait title for your projects! Click here. Click    
                 </h2>
                 <div class="border"></div>
-                
-                
             </a>
+            
         </div>
     </div>
 
