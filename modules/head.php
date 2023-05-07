2e9d7e2e50c1f643fac93a19513817c2ffc9bcd4
SMcGregor199 <shaynemcgregor1@gmail.com>
2023-05-07 12:28:55 -0400

    add meta image

--- modules/head.php
+++ modules/head.php
@@ -3,8 +3,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<title>Shayne McGregor's Developer Website</title>
-	<meta name="description" content="This is site features the work of Shayne McGregor">
-	<meta property="og:image" content="images/shayne.jpg">
+	<meta name="description" content="Shayne McGregor's Developer Portfolio">
+	<meta property="og:image" content="images/pixel-lion.jpg">
 
 	<link rel="stylesheet" href="style.css">
 	<link rel="stylesheet" href="https://use.typekit.net/hsy2cwu.css">
