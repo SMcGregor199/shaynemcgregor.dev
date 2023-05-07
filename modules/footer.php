7e5e2317b26f2ada41d9598b4a3760383bc678c8
SMcGregor199 <shaynemcgregor1@gmail.com>
2023-05-07 12:43:19 -0400

    add attribution to footer

--- modules/footer.php
+++ modules/footer.php
@@ -10,6 +10,7 @@
 					<a class="base-voice" target="new_window" href="https://github.com/SMcGregor199/personal-website"><span>projects</span></a>
 					<a class="base-voice" target="new_window" href="https://www.linkedin.com/in/shayne-mcgregor/"><span>contact</span></a>	
 				</nav>
+				© <a href='https://www.123rf.com/profile_captainvector'>captainvector</a>, <a href='https://www.123rf.com/free-images/'>123RF Free Images</a>
 			</div>
 		</inner-column>
 	</footer>
