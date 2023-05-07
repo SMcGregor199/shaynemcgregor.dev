e95389b0219981616ed9a95ebe475fb81f90f442
SMcGregor199 <shaynemcgregor1@gmail.com>
2023-05-07 12:54:40 -0400

    add attribution module

---
+++ modules/attribution.php
@@ -0,0 +1,3 @@
+<div class="attribution minor-voice">
+    © <a href='https://www.123rf.com/profile_captainvector'>captainvector</a>, <a href='https://www.123rf.com/free-images/'>123RF Free Images</a>
+</div>
