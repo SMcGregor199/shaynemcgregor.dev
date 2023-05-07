82ee794c14c27f2a6c34a8df4096d3b867953b52
SMcGregor199 <shaynemcgregor1@gmail.com>
2023-05-07 13:26:46 -0400

    …

--- modules/head.php
+++ modules/head.php
@@ -6,7 +6,7 @@
 	<meta name="description" content="Shayne McGregor's Developer Portfolio">
 	<meta property="og:image" content="/images/pixel-lion.jpg">
 
-	<link rel="stylesheet" href="style.css?refreshcss=1">
+	<link rel="stylesheet" href="style.css?refreshcss=2">
 	<link rel="stylesheet" href="https://use.typekit.net/hsy2cwu.css">
 	<link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
 </head>
