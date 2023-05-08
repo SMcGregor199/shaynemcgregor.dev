3359b9da33915708421f6f497169230e1f73477e
SMcGregor199 <shaynemcgregor1@gmail.com>
2023-05-08 16:29:44 -0400

    add link to codenewbie

--- modules/footer.php
+++ modules/footer.php
@@ -6,7 +6,7 @@
 			<div class="site-footer" style="border-top:1px solid var(--outlines); padding-top:25px;">
 				<nav class="site-nav">
 					<a class="base-voice" href="?page=home"><span>home</span></a>
-					<a class="base-voice" href="#"><span>articles</span></a>
+					<a class="base-voice" target="new_window" href="https://community.codenewbie.org/smcgregor199"><span>articles</span></a>
 					<a class="base-voice" target="new_window" href="https://github.com/SMcGregor199/personal-website"><span>projects</span></a>
 					<a class="base-voice" target="new_window" href="https://www.linkedin.com/in/shayne-mcgregor/"><span>contact</span></a>	
 				</nav>
