37a8beddd12ad4eee505f6f0fa313441fe42d3b4
SMcGregor199 <shaynemcgregor1@gmail.com>
2024-03-09 12:25:15 -0500

    remove project folder

--- projects/ecmascript modules/script.js
+++
@@ -1,6 +0,0 @@
-//Here we're using destructuring...remember that?
-import {double, tripple, render} from './library.js';
-
-const outlet = document.querySelector('span');
-
-render(outlet, double(10) + tripple(10)); // answer should be 50;
