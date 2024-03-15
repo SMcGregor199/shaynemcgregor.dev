24f78af7dca4605b78be4f179ce72690e49a4f58
SMcGregor199 <shaynemcgregor1@gmail.com>
2024-03-15 10:15:22 -0400

    testing post commands with cURL

--- test/index.php
+++ test/index.php
@@ -1,19 +1,51 @@
 <?php
-echo "This is a curl example";
+// echo "This is a curl example for getting data";
+
+// // initializes the connection
+// $ch = curl_init();
+
+// //establishes the connection with the specific url
+// curl_setopt($ch, CURLOPT_URL, 'https://randomuser.me/api/');
+
+// //retries the data from that server
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// //executing the curl command
+// $server_response = curl_exec($ch);
+
+// curl_close($ch);
+
+// echo "<pre>"; print_r($server_response); echo "</pre>";
+
+
+
+echo "This is a curl example for posting data";
+$userData = [
+	'client_name' => 'Dr. Test Person',
+	'client_address' => '123 Test Ave.'
+];
 
 // initializes the connection
 $ch = curl_init();
 
 //establishes the connection with the specific url
-curl_setopt($ch, CURLOPT_URL, 'https://randomuser.me/api/');
+
+
+//lets the cURL know that we're posting data
+curl_setopt($ch, CURLOPT_POST, true);
 
 //retries the data from that server
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
+//lets the cURL know what data we're posting
+curl_setopt($ch, CURLOPT_POSTFIELDS,$userData);
+
 //executing the curl command
-$server_response = curl_exec($ch);
+$result = curl_exec($ch);
 
 curl_close($ch);
 
-echo "<pre>"; print_r($server_response); echo "</pre>";
+echo $result;
+
+
 ?>
