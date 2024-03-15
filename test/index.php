b4494ddd971451f9eb2f179abd96fd2beb08b855
SMcGregor199 <shaynemcgregor1@gmail.com>
2024-03-15 09:42:49 -0400

    testing curl

--- test/index.php
+++ test/index.php
@@ -1,4 +1,19 @@
 <?php
 echo "This is a curl example";
 
+// initializes the connection
+$ch = curl_init();
+
+//establishes the connection with the specific url
+curl_setopt($ch, CURLOPT_URL, 'https://randomuser.me/api/');
+
+//retries the data from that server
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+//executing the curl command
+$server_response = curl_exec($ch);
+
+curl_close($ch);
+
+echo "<pre>"; print_r($server_response); echo "</pre>";
 ?>
